5abd959a28bc284dd6a8e484666bc14fce77a276
Clint Gunter <clint@clint.io>
2021-10-27 09:25:47 -0500

    Add URL for Airtable API

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 const API_URL_CATEGORIES = `https://api.fungenerators.com/name/categories.json?start=0&limit=25&api_key=${process.env.REACT_APP_NAME_API_KEY}`;
 const API_URL_NAMES_BEG = `https://api.fungenerators.com/name/generate?category=`;
 const API_URL_NAMES_END = `&limit=10&api_key=${process.env.REACT_APP_NAME_API_KEY}`;
-//const API_URL_AIRTABLE
+const API_URL_AIRTABLE = `https://api.airtable.com/v0/app3x4dnCfUephaZE/Table%201?api_key=${process.env.REACT_APP_AIRTABLE_API_KEY}`;
 
 function App() {
 
