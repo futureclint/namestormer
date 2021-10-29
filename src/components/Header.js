481806037706fbe0ad3fe5d60437777206195972
Clint Gunter <clint@clint.io>
2021-10-29 09:40:02 -0500

    Change link to use browser-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header>
-      <a href="/">
+      <Link to="/">
         <h1>Namestormer</h1>
-      </a>
+      </Link>
       <span className="emoji">&#9889;</span>
     </header>
   );
